a3845f45cd0995a9b07c28443b46db42d66fdbc3
KirillQA <lukinkirill22@gmail.com>
2024-10-31 18:05:54 +0300

    update successfulSearchTest

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,13 +1,13 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
+        $("[class=\"QS5gu sy4vM\"]").click();
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
